4d1f87da5661be63643be6a898f05b72050227cb
MSI\Lucas <lucasblon@orange.fr>
2017-10-18 17:11:24 +0200

    corriger erreur

--- Assets/Scripts/AttractObjBleue.cs
+++ Assets/Scripts/AttractObjBleue.cs
@@ -24,7 +24,7 @@ public class AttractObjBleue : MonoBehaviour {
      		UImanag.Instance().AddScore();
      	}
 
-        if (collision.gameObject.tag == other)
+        if (collision.gameObject.tag != "Bleu" )
         {
             Destroy(gameObject);
         }
