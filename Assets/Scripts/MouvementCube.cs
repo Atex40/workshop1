aeaf156c95f01982dc51bc2929d340ca573d634a
PCP-TRISTAN\trist <tristanlavaud@hotmail.fr>
2017-10-19 11:00:08 +0200

    Code fini plus qu'a passer en swip

--- Assets/Scripts/MouvementCube.cs
+++ Assets/Scripts/MouvementCube.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MouvementCube : MonoBehaviour {
-	float x;
+
+	private float x = 0;
+	private float y = 0;
+	private float z = 0;
+	private float time = 0.2f;
 
 	void Start () {
 	
@@ -11,13 +15,58 @@ public class MouvementCube : MonoBehaviour {
 	
 	void Update () {
 		Turn();
+		Turn2();
+		Turn3();
 	}
 
 	void Turn () {
 
 	if (Input.GetKeyDown("a")){
 		Debug.Log(Input.GetKeyDown("a"));
-//		transform.rotation = Quartenion(0,0,90);
+		StartCoroutine(WaitTime());
 		}	
 	}
+
+	IEnumerator WaitTime()
+    {
+    	z += 45;
+    	transform.rotation = Quaternion.Euler(x,y,z);
+        yield return new WaitForSeconds(time);
+        z += 45;
+        transform.rotation = Quaternion.Euler(x,y,z);
+    }
+
+    	void Turn2 () {
+
+	if (Input.GetKeyDown("z")){
+		Debug.Log(Input.GetKeyDown("z"));
+		StartCoroutine(WaitTime2());
+		}	
+	}
+
+	IEnumerator WaitTime2()
+    {
+    	y += 45;
+    	transform.rotation = Quaternion.Euler(x,y,z);
+        yield return new WaitForSeconds(time);
+        y += 45;
+        transform.rotation = Quaternion.Euler(x,y,z);
+    }
+
+        	void Turn3 () {
+
+	if (Input.GetKeyDown("e")){
+		Debug.Log(Input.GetKeyDown("e"));
+		StartCoroutine(WaitTime2());
+		}	
+	}
+
+	IEnumerator WaitTime3()
+    {
+    	x += 45;
+    	transform.rotation = Quaternion.Euler(x,y,z);
+        yield return new WaitForSeconds(time);
+        x += 45;
+        transform.rotation = Quaternion.Euler(x,y,z);
+    }
 }
