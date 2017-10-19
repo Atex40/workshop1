0cebf9e544ea13ed450ab6793f1005447e3b832e
Pierre <pierrebatisse33130@gmail.com>
2017-10-19 10:55:56 +0200

    Ajout systeme economique

---
+++ Assets/Scripts/EconomyManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EconomyManager : MonoBehaviour {
+
+    public int currentMoney;
+    public int addMoney;
+    public Text money;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        money.text = "$ " + currentMoney.ToString();
+
+	}
+
+    public void EarnMoney()
+    {
+        currentMoney = currentMoney + addMoney;
+    }
+
+    public void BuyItem1()
+    {
+        if (currentMoney > 500)
+        {
+            currentMoney = currentMoney - 500;
+        }
+    }
+
+    public void BuyItem2()
+    {
+        if (currentMoney > 1000)
+        {
+            currentMoney = currentMoney - 1000;
+        }
+    }
+
+    public void BuyItem3()
+    {
+        if (currentMoney > 2000)
+        {
+            currentMoney = currentMoney - 2000;
+        }
+    }
+}
