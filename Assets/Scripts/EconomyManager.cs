7e7eb998d4d737576888f93c099e89e038cf59fa
Pierre <pierrebatisse33130@gmail.com>
2017-10-20 12:13:41 +0200

    push MaJ Boutique

--- Assets/Scripts/EconomyManager.cs
+++ Assets/Scripts/EconomyManager.cs
@@ -8,9 +8,12 @@ public class EconomyManager : MonoBehaviour {
     private int currentMoney = 0;
     public int addMoney;
     public Text money;
-    private bool bi1 = false;
-    private bool bi2 = false;
-    private bool bi3 = false;
+    private int bi1YES = 0;
+    private int bi2YES = 0;
+    private int bi3YES = 0;
+    public GameObject buy1Button;
+    public GameObject buy2Button;
+    public GameObject buy3Button;
 
     private static EconomyManager instance;
     public static EconomyManager Instance()
@@ -32,17 +35,33 @@ public class EconomyManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
         currentMoney = PlayerPrefs.GetInt("currentMoney");
+        bi1YES = PlayerPrefs.GetInt("boolean1");
+        bi2YES = PlayerPrefs.GetInt("boolean2");
+        bi3YES = PlayerPrefs.GetInt("boolean3");
     }
 	
 	// Update is called once per frame
 	void Update () {
 
         money.text = currentMoney.ToString();
-        if (Input.GetKeyDown("c"))
+        if (Input.GetKeyDown("c")) // Ajoute monnaie au joueur
         {
             currentMoney = currentMoney + 250;
             Debug.Log("Plus de cash");
             PlayerPrefs.SetInt("currentMoney", currentMoney);
+        }
+
+        if (Input.GetKeyDown("v"))
+        {
+            currentMoney = 0;
+            bi1YES = 0;
+            bi2YES = 0;
+            bi3YES = 0;
+            PlayerPrefs.SetInt("currentMoney", currentMoney);
+            PlayerPrefs.SetInt("boolean1", bi1YES);
+            PlayerPrefs.SetInt("boolean2", bi2YES);
+            PlayerPrefs.SetInt("boolean3", bi3YES);
+            Debug.Log("monnaie reinitialisee");
         }
 
 	}
@@ -51,10 +70,12 @@ public class EconomyManager : MonoBehaviour {
 
     public void BuyItem1()
     {
-        if (currentMoney >= 500 && bi1 == false)
+        if (currentMoney >= 500 && bi1YES == 0)
         {
             currentMoney = currentMoney - 500;
-            bi1 = true;
+            bi1YES = 1;
+            PlayerPrefs.SetInt("currentMoney", currentMoney);
+            PlayerPrefs.SetInt("boolean1", bi1YES);
             Debug.Log("Item 1 acheté");
         }
         else
@@ -65,10 +86,12 @@ public class EconomyManager : MonoBehaviour {
 
     public void BuyItem2()
     {
-        if (currentMoney >= 1000 && bi2 == false)
+        if (currentMoney >= 1000 && bi2YES == 0)
         {
             currentMoney = currentMoney - 1000;
-            bi2 = true;
+            bi2YES = 1;
+            PlayerPrefs.SetInt("currentMoney", currentMoney);
+            PlayerPrefs.SetInt("boolean2", bi2YES);
             Debug.Log("Item 2 acheté");
         }
         else
@@ -79,10 +102,12 @@ public class EconomyManager : MonoBehaviour {
 
     public void BuyItem3()
     {
-        if (currentMoney >= 2000 && bi3 == false)
+        if (currentMoney >= 2000 && bi3YES == 0)
         {
             currentMoney = currentMoney - 2000;
-            bi3 = true;
+            bi3YES = 1;
+            PlayerPrefs.SetInt("currentMoney", currentMoney);
+            PlayerPrefs.SetInt("boolean3", bi3YES);
             Debug.Log("Item 3 acheté");
         }
         else
