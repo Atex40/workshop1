d134bf8205af1d9e61d59bb40a30bc9b4ba27a1e
PCP-TRISTAN\trist <tristanlavaud@hotmail.fr>
2017-10-20 10:26:23 +0200

    debut highscore

---
+++ Assets/Scripts/Highscore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore : MonoBehaviour {
+
+	public Text score;
+	public Text highscore;
+
+	void Start () 
+	{
+		highscore.text = PlayerPrefs.GetInt("Highscore",0).ToString();
+	}
+	
+	void Update () 
+	{
+		int number = Random.Range(1, 7);
+		score.text = number.ToString();
+
+		if (number > PlayerPrefs.GetInt("Highscore", 0))
+		{
+			PlayerPrefs.SetInt("Highscore", number);
+			highscore.text = number.ToString();
+		}
+	}
+}
